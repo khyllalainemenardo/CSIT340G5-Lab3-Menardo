b24d01a7caa823a4200056feb93e7a3d3f75ddf6
khyllalainemenardo <khyllalaine.menardo@cit.edu>
2026-09-21 20:53:17 +0800

    1.3: course information step 3

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const Header = (props) => {
 const Part = (props) => {
   return (
     <p>
-      {props.part} {props.exercises}
+      {props.part.name} {props.part.exercises}
     </p>
   )
 }
@@ -13,9 +13,9 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
+      <Part part={props.part1} />
+      <Part part={props.part2} />
+      <Part part={props.part3} />
     </div>
   )
 }
@@ -36,13 +36,18 @@ const Footer = (props) => {
 
 const App = () => {
   const course = 'CSIT340 Industry Elective 1'
-  const part1 = 'IT332 Capstone and Research 1'
-  const exercises1 = 3
-  const part2 = 'CSIT327 Information Management 2'
-  const exercises2 = 3
-  const part3 = 'IT365 Data Analytics'
-  const exercises3 = 3
-
+  const part1 = {
+    name: 'IT332 Capstone and Research 1',
+    exercises: 3
+  }
+  const part2 = {
+    name: 'CSIT327 Information Management 2',
+    exercises: 3
+  }
+  const part3 = {
+    name: 'IT365 Data Analytics',
+    exercises: 3
+  }
   const studentName = 'Khylla Laine C. Menardo'
   const courseCode = 'CSIT340'
   const section = 'G05'
@@ -50,15 +55,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={part1}
-        exercises1={exercises1}
-        part2={part2}
-        exercises2={exercises2}
-        part3={part3}
-        exercises3={exercises3}
-      />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
       <Footer name={studentName} courseCode={courseCode} section={section} />
     </div>
   )
